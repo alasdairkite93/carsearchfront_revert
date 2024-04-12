2c635bb84420d869634db23166b0831e3d140e9c
alasdairkite93 <alasdairkite93@gmail.com>
2024-04-12 21:25:06 +0100

    Updated header

--- src/components/header.js
+++ src/components/header.js
@@ -12,9 +12,8 @@ export default function Header() {
         <div className="top-element">
             <div className="middle-element">
             <div className="two">
-                        <h1 id="headertext">PCN.UK</h1>
+                        <h1 id="headertext">PCN Checker</h1>
                         <h2 id="headertitle">Get a PCN reminder</h2>
-                        <h2 id="sitewarn">This is NOT a GOV.UK site</h2>
                     {/*<div className="threeheadertext">*/}
                     {/*    <h1 id="notgovsite">This is not a GOV.UK site</h1>*/}
                     {/*</div>*/}
